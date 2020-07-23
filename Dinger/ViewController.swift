d31f709f53f82e165b906ec2929c7af42e7b9518
Rodrigo Conte <rodrigo.conte14@gmail.com>
2020-07-23 13:50:29 -0400

    Initial Commit

---
+++ Dinger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dinger
+//
+//  Created by Rodrigo Conte on 23/07/20.
+//  Copyright © 2020 Rodrigo Conte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
